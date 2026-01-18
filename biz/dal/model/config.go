c0c87b18f58bb9683e022ee57380b2f8f63186be
murphyyi <zy84338719@hotmail.com>
2026-01-18 12:36:42 +0800

    保存

--- biz/dal/model/config.go
+++ biz/dal/model/config.go
@@ -17,7 +17,6 @@ type Config struct {
 	ResourceKey    string         `gorm:"column:resource_key;uniqueIndex:uk_config_resource,priority:3" json:"resource_key,omitempty"`
 	Alias          string         `gorm:"column:alias;index:idx_config_alias" json:"alias,omitempty"`
 	Name           string         `gorm:"column:name" json:"name,omitempty"`
-	BusinessKey    string         `gorm:"column:business_key;index:idx_config_business" json:"business_key,omitempty"`
 	Content        string         `gorm:"column:content;type:text" json:"content,omitempty"`
 	Type           string         `gorm:"column:type" json:"type,omitempty"`
 	Remark         string         `gorm:"column:remark;type:varchar(512)" json:"remark,omitempty"`
